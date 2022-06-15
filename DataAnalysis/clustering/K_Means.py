9b2444740b6cd4ec6dee71715e782346848e49f6
stigGGGer <shikgyla@gmail.com>
2022-06-15 23:57:16 +0300

    финалОчка

--- DataAnalysis/clustering/K_Means.py
+++ DataAnalysis/clustering/K_Means.py
@@ -9,7 +9,5 @@ class covariance_type(Enum):
     spherical = "spherical"
     
 def K_Means(table, parametrs):
-    kmeans = KMeans(n_clusters=parametrs[0], random_state=parametrs[2]).fit(table)
-    #gm = GaussianMixture(n_components=parametrs[0],covariance_type = parametrs[1], random_state=parametrs[2]).fit(table)
-    #gm.predict([[0, 0], [12, 3]])
+    kmeans = KMeans(n_clusters=parametrs[0], random_state=parametrs[2]).fit(table)    
     return [table,kmeans.labels_]
